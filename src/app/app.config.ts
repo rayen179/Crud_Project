5c2993725b9424cec30ca6bfe0b7886ad46cf84f
rayencherni589@gmail.com <rayencherni589@gmail.com>
2024-06-27 14:17:41 +0200

    feat: product interfaces

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,40 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { tokenHttpInterceptor } from './token-http-interceptor';
+
+import {
+  GoogleLoginProvider,
+  SocialAuthServiceConfig,
+} from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withInterceptors([tokenHttpInterceptor])),
+    provideToastr(),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('Your_GoogleApp_ClientID', {
+              oneTapEnabled: false,
+              prompt: 'consent',
+            }),
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        },
+      } as SocialAuthServiceConfig,
+    },
+  ],
 };
